cd875f506af90b15cb8865814e1b9b544f037fb5
Kevin Chen <kchen1250@gmail.com>
2022-05-04 23:25:51 -0400

    Simplify for sake of simplification. only doing this because I can rely on github history to save me if this doesn't actually work

--- src/Grid/StaggeredGrid.cpp
+++ src/Grid/StaggeredGrid.cpp
@@ -127,36 +127,23 @@ const float StaggeredGrid::operator()(float x, float y) const
       {
         if (yi == 0)
           return _data(xi, 1);
-        else if (yi == _y_res * 2)
+        if (yi == _y_res * 2)
           return _data(xi, _y_res * 2 - 1);
-        else
-          return (_data(xi, yi-1) + _data(xi, yi+1)) * 0.5f;
-      }
-      else
-      {
-        if (xi == 0)
-          return _data(0, yi);
-        else if (xi == _x_res)
-          return _data(_x_res-1, yi);
-        else
-          return (_data(xi-1, yi) + _data(xi, yi)) * 0.5f;
-      }
-    }
-    else
-      return _data(xi, yi);
-  }
-  else
-  {
-    if (y_border)
-      return _data(xi, yi);
-    else
-    {
-      if (_axis)
         return (_data(xi, yi-1) + _data(xi, yi+1)) * 0.5f;
-      else
-        return (_data(xi, yi) + _data(xi+1, yi)) * 0.5f;
+      }
+      if (xi == 0)
+        return _data(0, yi);
+      if (xi == _x_res)
+        return _data(_x_res-1, yi);
+      return (_data(xi-1, yi) + _data(xi, yi)) * 0.5f;
     }
+    return _data(xi, yi);
   }
+  if (y_border)
+    return _data(xi, yi);
+  if (_axis)
+    return (_data(xi, yi-1) + _data(xi, yi+1)) * 0.5f;
+  return (_data(xi, yi) + _data(xi+1, yi)) * 0.5f;
 }
 
 const float StaggeredGrid::get(float x, float y)
